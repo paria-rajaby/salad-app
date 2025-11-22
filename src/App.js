c6dd76e4c2eb4d24b237a882e063f5527b71d132
Paria Rajaby <pariarajaby2004@gmail.com>
2025-11-22 20:04:40 +0330

    add main section and fetch datas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Components/Header/Header";
+import Main from "./Components/Main/Main";
+import { useEffect, useState } from "react";
 
 function App() {
+  const key = process.env.REACT_APP_SUPABASE_KEY;
+  const url = process.env.REACT_APP_SUPABASE_URL;
+
+  const [popular, setPopular] = useState([]);
+  const [vegan, setVegan] = useState([]);
+  const [meat, setMeat] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        `${url}/rest/v1/salads?select=*&order=created_at.asc`,
+        {
+          method: "GET",
+          headers: {
+            apikey: key,
+            Authorization: `Bearer ${key}`,
+          },
+        }
+      );
+      const data = await response.json();
+
+      const popularSalad = data.filter((salad) => salad.popular === true);
+      const veganSalad = data.filter((salad) => salad.category == "vegan");
+      const meatSalad = data.filter((salad) => salad.category == "meat");
+      setPopular(popularSalad);
+      setVegan(veganSalad);
+      setMeat(meatSalad);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="container">
       <header>
         <Header />
       </header>
+      <main>
+        <Main title="Top List" desc="Try our best items" items={popular} />
+        <Main title="Vegan Salad" desc="Fresh vegan salads" items={vegan} />
+        <Main title="Meat Salad" desc="Fresh meat salads" items={meat} />
+      </main>
     </div>
   );
 }
---
+++ src/Components/Main/Main.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import "./Main.css";
+import { IoAddOutline } from "react-icons/io5";
+
+import { Navigation } from "swiper/modules";
+
+export default function Main({ title, desc, items }) {
+  return (
+    <div className="main">
+      <div className="main-info">
+        <h2 className="main-info_title">{title}</h2>
+        <p className="main-info_desc">{desc}</p>
+      </div>
+      <div className="main-products">
+        <Swiper
+          className="swiper"
+          spaceBetween={20}
+          slidesPerView={3}
+          loop={true}
+          navigation={true}
+          centeredSlides={true}
+          modules={[Navigation]}
+        >
+          {items.map((item) => (
+            <SwiperSlide>
+              <div className="salad-wrapper">
+                <img className="salad-image" src={item.image} alt="" />
+                <div className="salad-infos">
+                  <span>{item.name}</span>
+                  <p>{item.description}</p>
+                </div>
+                <div className="salad-buy_section">
+                  <span>{item.price}</span>
+                  <button>
+                    <IoAddOutline />
+                  </button>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
+}
